270ac7dee17f7c43604f619ef4c6121f32748ef6
Karl Broman <kbroman@gmail.com>
2014-07-11 14:33:41 -0400

    Remove the data scientist bit

--- JS/slide_transition.js
+++ JS/slide_transition.js
@@ -10,24 +10,7 @@ slide_transition = function(slidenumber) {
   if(slidenumber==3) {
     d3.select("font#fadeout_applied").transition().duration(3000).ease("linear").style("opacity", 0.2);
   }
-  if(slidenumber==2) {
+  if(slidenumber==2 || slidenumber==4) {
     d3.select("font#fadeout_applied").style("opacity", 1);
   }
-  if(slidenumber==4) {
-    d3.select("font#fadeout_applied").style("opacity", 0.2);
-  }
-
-  if(slidenumber==4) {
-    d3.select("h3#fadeout_appliedstat").transition().duration(2000).ease("linear").style("opacity", 0)
-    d3.select("h3#fadein_datasci").transition().delay(1500).duration(4000).ease("linear").style("opacity", 1);
-  }
-  if(slidenumber==3) {
-    d3.select("h3#fadeout_appliedstat").style("opacity", 1);
-    d3.select("h3#fadein_datasci").style("opacity", 0);
-  }
-  if(slidenumber==5) {
-    d3.select("h3#fadeout_appliedstat").style("opacity", 0);
-    d3.select("h3#fadein_datasci").style("opacity", 1);
-  }
-
 }
