e57cba3da9aea3b4bdda071f98c632a8f5c516c6
Karl Broman <kbroman@gmail.com>
2014-10-31 17:53:18 -0500

    Revise slide_transition.js for delayed appearance of "dont forget .enter()"

--- JS/slide_transition.js
+++ JS/slide_transition.js
@@ -7,15 +7,7 @@ slide_transition = function(slidenumber) {
   current_slide = slidenumber;
 
 
-  if(slidenumber==3) {
-    d3.select("font#fadeout_applied").transition().duration(3000).ease("linear").style("opacity", 0.2);
-  }
-  if(slidenumber==2 || slidenumber==4) {
-    d3.select("font#fadeout_applied").style("opacity", 1);
-  }
-
-
-  if(slidenumber==15) {
+  if(slidenumber==31) {
     d3.select("p#dotenter").transition()
                            .style("opacity", 1)
                            .delay(20000)
