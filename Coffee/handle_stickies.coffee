9371fe71a559aa92b31d6eb661047c9304817220
Karl Broman <kbroman@gmail.com>
2014-11-04 18:56:25 -0600

    handle_stickies: after removing stickies, make "show stickies" text only viewed on mouseover

--- Coffee/handle_stickies.coffee
+++ Coffee/handle_stickies.coffee
@@ -6,12 +6,18 @@ handle_stickies = () ->
             if(stickies_on)
                 stickies_on = false
                 d3.select(this).text("Show stickies")
+                               .on("mouseover", () ->
+                                   d3.select(this).style("opacity", 1))
+                               .on("mouseout", () ->
+                                   d3.select(this).style("opacity", 0))
                 d3.selectAll("aside").style("opacity", 0)
                                      .style("pointer-events", "none")
                 d3.selectAll("a").style("border-bottom", "none")
             else
                 stickies_on = true
                 d3.select(this).text("Remove stickies")
+                               .on("mouseover", () -> [])
+                               .on("mouseout", () -> [])
                 d3.selectAll("aside").style("opacity", 1)
                                      .style("pointer-events", "auto")
                 d3.selectAll("a").style("border-bottom", "")
--- JS/handle_stickies.js
+++ JS/handle_stickies.js
@@ -7,12 +7,20 @@ handle_stickies = function() {
   return d3.select("p#remove_stickies").on("click", function() {
     if (stickies_on) {
       stickies_on = false;
-      d3.select(this).text("Show stickies");
+      d3.select(this).text("Show stickies").on("mouseover", function() {
+        return d3.select(this).style("opacity", 1);
+      }).on("mouseout", function() {
+        return d3.select(this).style("opacity", 0);
+      });
       d3.selectAll("aside").style("opacity", 0).style("pointer-events", "none");
       return d3.selectAll("a").style("border-bottom", "none");
     } else {
       stickies_on = true;
-      d3.select(this).text("Remove stickies");
+      d3.select(this).text("Remove stickies").on("mouseover", function() {
+        return [];
+      }).on("mouseout", function() {
+        return [];
+      });
       d3.selectAll("aside").style("opacity", 1).style("pointer-events", "auto");
       return d3.selectAll("a").style("border-bottom", "").style("border-bottom", "dotted 1px #555;");
     }
